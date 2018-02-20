92e89e050943a13c9ccd9663e90ac5a7d934fef0
Geocarlos Lopes Alves <geowildcat@gmail.com>
2018-02-20 01:28:56 -0400

    Add feature to show storylines

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -20,6 +20,7 @@ main_page_head = '''
         body {
             padding-top: 80px;
         }
+
         #trailer .modal-dialog {
             margin-top: 200px;
             width: 640px;
@@ -80,7 +81,23 @@ main_page_head = '''
           $('.movie-tile').hide().first().show("fast", function showNext() {
             $(this).next("div").show("fast", showNext);
           });
+
+          $(".storyline").hide();
+
+          $('#view-storylines').click(()=>{
+            const text = $("#view-storylines").text();
+            console.log(text)
+            if(text === "Hide Storylines"){
+                $(".storyline").hide();
+                $("#view-storylines").text("View Storylines");
+                return;
+            }
+            $(".storyline").show();
+            $("#view-storylines").text("Hide Storylines");
+          })
+
         });
+
     </script>
 </head>
 '''
@@ -109,6 +126,7 @@ main_page_content = '''
           <div class="navbar-header">
             <a class="navbar-brand" href="#">Trailers of My Favorite Movies</a>
           </div>
+          <div class="text-right"><a id="view-storylines" class="btn btn-link btn-sm">View Storylines</a></div>
         </div>
       </div>
     </div>
@@ -125,6 +143,7 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <p class="storyline">{movie_storyline}</p>
 </div>
 '''
 
@@ -144,6 +163,7 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
+            movie_storyline=movie.storyline,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
