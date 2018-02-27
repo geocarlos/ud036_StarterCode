97e2a8bdaa6a89c2a98aaa70cc44071f31405a1c
Geocarlos Lopes Alves <geowildcat@gmail.com>
2018-02-27 13:50:51 -0400

    Fix code formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,62 +3,70 @@ from fresh_tomatoes import open_movies_page
 
 """
 List of movies.
-Please check Movie class in media.py file for clarification on how the Movie objects are constructed.
+Please check Movie class in media.py file for clarification on how the Movie
+objects are constructed.
 """
 movies = [
 
     Movie("Interstellar",
-    "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-    "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
-    "https://youtu.be/zSWdZVtXT7E"),
+          '''A team of explorers travel through a wormhole in space in an
+          attempt to ensure humanity's survival.''',
+          "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+          "https://youtu.be/zSWdZVtXT7E"),
 
     Movie("The Matrix",
-    '''A computer hacker learns from mysterious rebels about the true nature of his reality and his
-    role in the war against its controllers. ''',
-    "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-    "https://youtu.be/m8e-FF8MsqU"),
+          '''A computer hacker learns from mysterious rebels about the true nature
+          of his reality and his role in the war against its controllers. ''',
+          "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+          "https://youtu.be/m8e-FF8MsqU"),
 
     Movie("The Terminator",
-    '''A seemingly indestructible humanoid cyborg is sent from 2029 to 1984 to assassinate a waitress,
-    whose unborn son will lead humanity in a war against the machines, while a soldier from that war is
-    sent to protect her at all costs.''',
-    "https://upload.wikimedia.org/wikipedia/en/7/70/Terminator1984movieposter.jpg",
-    "https://youtu.be/rBlT3eXrXZU"),
+          '''A seemingly indestructible humanoid cyborg is sent from 2029 to 1984
+          to assassinate a waitress, whose unborn son will lead humanity in a war
+          against the machines, while a soldier from that war is sent to protect
+          her at all costs.''',
+          "https://upload.wikimedia.org/wikipedia/en/7/70/Terminator1984movieposter.jpg",
+          "https://youtu.be/rBlT3eXrXZU"),
 
     Movie("Pink Floyd - The Wall",
-    '''A confined but troubled rock star descends into madness in the midst of his physical and social
-    isolation from everyone.''',
-    "https://upload.wikimedia.org/wikipedia/en/9/94/Pink_Floyd_The_Wall.jpg",
-    "https://youtu.be/5CJRnRyA4wA"),
+          '''A confined but troubled rock star descends into madness in the midst
+          of his physical and social isolation from everyone.''',
+          "https://upload.wikimedia.org/wikipedia/en/9/94/Pink_Floyd_The_Wall.jpg",
+          "https://youtu.be/5CJRnRyA4wA"),
 
     Movie("Abre Los Ojos",
-    '''A very handsome man finds the love of his life, but he suffers an accident and needs to have his
-    face rebuilt by surgery after it is severely disfigured..''',
-    "https://upload.wikimedia.org/wikipedia/en/a/a6/Abre_los_ojos_movie.jpg",
-    "https://youtu.be/PBtnPuB0x3U"),
+          '''A very handsome man finds the love of his life, but he suffers an
+          accident and needs to have his face rebuilt by surgery after it is
+          severely disfigured..''',
+          "https://upload.wikimedia.org/wikipedia/en/a/a6/Abre_los_ojos_movie.jpg",
+          "https://youtu.be/PBtnPuB0x3U"),
 
     Movie("Transcendence",
-    '''A scientist's drive for artificial intelligence, takes on dangerous implications when his consciousness
-    is uploaded into one such program.''',
-    "https://upload.wikimedia.org/wikipedia/en/e/ef/Transcendence2014Poster.jpg",
-    "https://youtu.be/QheoYw1BKJ4"),
+          '''A scientist's drive for artificial intelligence, takes on dangerous
+          implications when his consciousness is uploaded into one such program.''',
+          "https://upload.wikimedia.org/wikipedia/en/e/ef/Transcendence2014Poster.jpg",
+          "https://youtu.be/QheoYw1BKJ4"),
 
     Movie("Resident Evil",
-    '''A special military unit fights a powerful, out-of-control supercomputer and hundreds of scientists who have mutated into flesh-eating creatures after a laboratory accident.''',
-    "https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Resident_evil_ver4.jpg/220px-Resident_evil_ver4.jpg",
-    "https://youtu.be/PWUT4CXWcwQ"),
+          '''A special military unit fights a powerful, out-of-control supercomputer
+          and hundreds of scientists who have mutated into flesh-eating creatures
+          after a laboratory accident.''',
+          "https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Resident_evil_ver4.jpg/220px-Resident_evil_ver4.jpg",
+          "https://youtu.be/PWUT4CXWcwQ"),
 
     Movie("Non Ti Muovere",
-    '''Gaetano and Delia, a separated couple, try to pick up the pieces of their broken love,
-    recalling all the faults and the mistakes which led them to where they are now.''',
-    "https://upload.wikimedia.org/wikipedia/en/5/5d/Don%27t_Move%282004%29.jpg",
-    "https://youtu.be/Wn4V8BXzjNI"),
+          '''Gaetano and Delia, a separated couple, try to pick up the pieces
+          of their broken love, recalling all the faults and the mistakes which
+          led them to where they are now.''',
+          "https://upload.wikimedia.org/wikipedia/en/5/5d/Don%27t_Move%282004%29.jpg",
+          "https://youtu.be/Wn4V8BXzjNI"),
 
     Movie("Tropa de Elite",
-    ''' Captain Nascimento has to find a substitute for his occupation while
-    trying to take down drug dealers and criminals before the Pope comes to Rio de Janeiro, Brazil.''',
-    "https://upload.wikimedia.org/wikipedia/en/2/2a/TropaDeElitePoster.jpg",
-    "https://youtu.be/ELlkWtNWyKY")
+          ''' Captain Nascimento has to find a substitute for his occupation while
+          trying to take down drug dealers and criminals before the Pope comes to
+          Rio de Janeiro, Brazil.''',
+          "https://upload.wikimedia.org/wikipedia/en/2/2a/TropaDeElitePoster.jpg",
+          "https://youtu.be/ELlkWtNWyKY")
 ]
 
 open_movies_page(movies)
